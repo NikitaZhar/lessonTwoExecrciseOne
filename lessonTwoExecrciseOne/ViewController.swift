3b6f27428c6eb0af732e7c80116907fa0cfe6e51
Nikita Zharinov <nikita.zharinov@gmail.com>
2021-01-29 15:24:45 +0100

    Basic Outlets

--- lessonTwoExecrciseOne/ViewController.swift
+++ lessonTwoExecrciseOne/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var coloredView: UIView!
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func redColorAction(_ sender: Any) {
+    }
+    
+    @IBAction func greenColorAction(_ sender: Any) {
+    }
+    
+    @IBAction func blueColorAction(_ sender: Any) {
+    }
+    
 }
 
